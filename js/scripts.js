a51446ec9887cf3f3b8d3ef892eeb80a3d8cce8d
Begli0102 <begli.kaka@mail.ru>
2021-02-06 16:27:22 +0100

    Update scripts.js
    added  new pokemon list for future project

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
-alert('Hello world');
-var favoriteFood='Spaghetti';
-document.write(favoriteFood);
+let pokemonList=[
+  {name="Bulbasaur", height = 0.7m, types=['grass', 'poison'], weight=6.9kg},
+  {name="Pidgeot", height = 1.5m, types=['flying', 'normal'], weight=39.5kg},
+  {name="Alakazam", height = 1.5m, types=['phychic'], weight=48kg},
+  {name="Charizard", height = 1.7m, types=['flying', 'fire'], weight=90.5kg},
+  {name="Blastoise", height = 1.6m, types=['water'], weight=85.5kg},
+];
