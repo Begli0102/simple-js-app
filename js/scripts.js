09349f2f4a887d134b9e7d8a157efcb3f911c570
Begli0102 <begli.kaka@mail.ru>
2021-02-09 23:53:00 +0100

    Update scripts.js
    created for loop and indicated  pokemons according to their height

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-//added some data to pokemonList 
+//added some data to pokemonList
 let pokemonList=[
  {name:'Bulbasaur',height: 0.7,types:['grass', 'poison' ],weight:6.9},
  {name:'Pidgeot',height:1.5,types:['flying', 'normal'],weight:39.5},
@@ -6,3 +6,10 @@ let pokemonList=[
  {name:'Charizard',height:1.7,types:['flying', 'fire'],weight:90.5},
  {name:'Blastoise',height:1.6,types:['water'],weight:85.5}
 ]
+for (let i=0; i<pokemonList.length; i++){
+  if(pokemonList[i].height>1.5){
+  console.log(pokemonList[i].name + "" + " ( height: " + pokemonList[i].height + ")");
+} else{
+  console.log(pokemonList[i].name + "" + " (height: " + pokemonList[i].height + ":this pokemon is small)");
+}
+}
