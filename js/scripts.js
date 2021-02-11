f95d1874ec9e020094b7c93d9dd4a0f2ffe4431d
Begli0102 <begli.kaka@mail.ru>
2021-02-12 00:36:35 +0100

    Update scripts.js
    added a paragraph tag to the output

--- js/scripts.js
+++ js/scripts.js
@@ -8,10 +8,10 @@ let pokemonList=[
 ]
 for (let i=0; i<pokemonList.length; i++){
   if(pokemonList[i].height>1.5){
-  document.write(pokemonList[i].name + "" +
-   " ( height: " + pokemonList[i].height + ")");
+  document.write('<p>' + pokemonList[i].name + "" +
+   " ( height: " + pokemonList[i].height + ")" + '</p>');
 } else{
-  document.write(pokemonList[i].name + "" +
-  " (height: " + pokemonList[i].height + ":this pokemon is small)");
+  document.write('<p>' + pokemonList[i].name + "" +
+  " (height: " + pokemonList[i].height + ":this pokemon is small)" + '</p>');
 }
 }
